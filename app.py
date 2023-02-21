af847e29e2cc3cfbf98e16959a1bd48f2f7a4b71
OmarElDwary <105021882+OmarElDwary@users.noreply.github.com>
2023-02-21 16:36:15 +0200

    3rd commit

--- app.py
+++ app.py
@@ -2,10 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'San Francisco, CA',
+        'company': 'Google',
+        'category': 'Software',
+        'description': 'We are looking for a software engineer to join our team.'
+    },
+    {
+        'id': 2,
+        'title': 'Software Engineer',
+        'location': 'Hamburg, Germany',
+        'company': 'Facebook',
+        'category': 'Software',
+        'description': 'We are looking for a software engineer to join our team.'
+    },
+    {
+        'id': 3,
+        'title': 'Cloud Engineer',
+        'location': 'Berlin, Germany',
+        'company': 'Amazon',
+        'category': 'Software',
+        'description': 'We are looking for a cloud engineer to join our team.'
+    }   
+]
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def hello_dawdaw():
+    return render_template("index.html", 
+                           jobs=JOBS)
+    
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
